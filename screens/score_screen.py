5b26b7e7433f614ddc5113e38f865c8167b32a93
luis_calvo <luiscalvo80@hotmail.com>
2024-11-18 15:18:42 +0100

    score screen started, indentation issue

--- screens/score_screen.py
+++ screens/score_screen.py
@@ -1,12 +1,33 @@
-# screens/score_screen.py
 from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty, ListProperty, NumericProperty
 
 class ScoreScreen(Screen):
     player_name = StringProperty("Player")
-    hole_scores = ListProperty([0] * 18)
-    total_score = NumericProperty(0)
+    hole_scores = ListProperty([0] * 18)  # Scores for each hole (18 holes)
+    total_score = NumericProperty(0)  # Total score across all holes
+    course_data = ListProperty([{'distance': 0, 'par': 0, 'handicap': 0}] * 18)  # List of dicts containing distance, par, and handicap for each hole
+    course_name = StringProperty("Course Name")
 
-    def update_score(self, hole, score):
-        self.hole_scores[hole - 1] = score
+    def update_score(self, hole_number, score):
+        """Update the score for a given hole and recalculate the total score."""
+        self.hole_scores[hole_number - 1] = score
         self.total_score = sum(self.hole_scores)
+
+    def set_course_data(self, course_data):
+        """Set course data (distance, par, and handicap for each hole)."""
+        self.course_data = course_data
+
+    def set_course(self, course):
+        """Set the course data for a specific course."""
+        self.course_name = course['course_name']
+        course_data = []
+        
+        # Adjust course data to match the hole data structure
+        for hole in course['holes']:
+            course_data.append({
+                'distance': hole['distance']['white'],  # Can choose 'white', 'yellow', etc.
+                'par': hole['par'],
+                'handicap': hole['handicap']
+            })
+        
+        self.set_course_data(course_data)
